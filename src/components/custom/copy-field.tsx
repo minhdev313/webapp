5165bc32db1fecba81bd3c3f0c69eaba9bb8b390
Ly Le <lecongly28102002@gmail.com>
2024-10-11 20:51:32 +0700

    feat: 🎸 add data table & custom components

---
+++ src/components/custom/copy-field.tsx
@@ -0,0 +1,32 @@
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+import { CopyButton } from "./copy-button";
+
+export function CopyField(props: { value: string, label?: React.ReactNode, helper?: React.ReactNode, height?: number, monospace?: boolean }) {
+  return (
+    <div>
+      {props.label && (
+        <Label>
+          {props.label}
+        </Label>
+      )}
+      <div className="relative pr-2">
+        <Textarea
+          readOnly
+          value={props.value}
+          style={{
+            height: props.height,
+            fontFamily: props.monospace ? "ui-monospace, monospace" : "inherit",
+            whiteSpace: props.monospace ? "pre" : "normal",
+          }}
+        />
+        <CopyButton content={props.value} className="absolute right-4 top-2" />
+      </div>
+      {props.helper && (
+        <Label>
+          {props.helper}
+        </Label>
+      )}
+    </div>
+  );
+}
---
+++ src/components/data-table/column-header.tsx
@@ -0,0 +1,73 @@
+import { cn } from "@/lib/utils";
+import { Column } from "@tanstack/react-table";
+import { ArrowDown, ArrowUp, EyeOff, LucideIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+interface DataTableColumnHeaderProps<TData, TValue>
+  extends React.HTMLAttributes<HTMLDivElement> {
+  column: Column<TData, TValue>;
+  columnTitle: React.ReactNode;
+}
+
+function Item(props: {
+  icon: LucideIcon;
+  onClick: () => void;
+  children: React.ReactNode;
+}) {
+  return (
+    <DropdownMenuItem onClick={props.onClick}>
+      <div className="flex items-center">
+        <props.icon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+        {props.children}
+      </div>
+    </DropdownMenuItem>
+  );
+}
+
+export function DataTableColumnHeader<TData, TValue>({
+  column,
+  columnTitle,
+  className,
+}: DataTableColumnHeaderProps<TData, TValue>) {
+  return (
+    <div className={cn("flex items-center space-x-2", className)}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="-ml-3 h-8 data-[state=open]:bg-accent"
+          >
+            <span>{columnTitle}</span>
+            {column.getIsSorted() === "desc" ? (
+              <ArrowDown className="ml-2 h-4 w-4" />
+            ) : column.getIsSorted() === "asc" ? (
+              <ArrowUp className="ml-2 h-4 w-4" />
+            ) : null}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          {column.getCanSort() && (
+            <>
+              <Item icon={ArrowUp} onClick={() => column.toggleSorting(false)}>
+                Asc
+              </Item>
+              <Item icon={ArrowDown} onClick={() => column.toggleSorting(true)}>
+                Desc
+              </Item>
+            </>
+          )}
+          <Item icon={EyeOff} onClick={() => column.toggleVisibility(false)}>
+            Hide
+          </Item>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
